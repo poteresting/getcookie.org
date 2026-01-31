b80d81ed4d94135f306439fbbe444616cb34b3ac
Poteresting <poteresting@gmail.com>
2026-01-31 17:25:41 +0200

    animation and links

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const LINKS = {
   DISCORD_BOT: 'https://discord.com/oauth2/authorize?client_id=1465030288659710235',
-  DISCORD_SUPPORT: 'https://discord.gg/getcookie',
+  DISCORD_SUPPORT: 'https://discord.gg/a2BQQaNp',
   TWITTER: 'https://x.com/GetCookieHQ',
 } as const;
