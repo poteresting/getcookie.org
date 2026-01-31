2e88a27c096b7a02c58e52daebfbab635dbd6b43
Poteresting <poteresting@gmail.com>
2026-01-31 17:32:10 +0200

    discord add link

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const LINKS = {
-  DISCORD_BOT: 'https://discord.com/oauth2/authorize?client_id=1465030288659710235',
+  DISCORD_BOT: 'https://discord.com/oauth2/authorize?client_id=1466162578404151483',
   DISCORD_SUPPORT: 'https://discord.gg/a2BQQaNp',
   TWITTER: 'https://x.com/GetCookieHQ',
 } as const;
